c9ef535868a7437dbcb6d08f475d652675ba860a
Rafael Miralha <rafamiralha1@gmail.com>
2024-08-29 09:45:03 -0300

    fix: NavBar estava com CSS em conflito

--- src/App.js
+++ src/App.js
@@ -3,24 +3,26 @@ import { BrowserRouter, Routes, Route, Link} from 'react-router-dom';
 import Home from './components/Home';
 import Contact from './components/Contact';
 
+
 //está é a função core do meu sistema, ela está armazenando as rotas e o conteúdo fixo da minha aplicação.
 function App() {
   return (
     <div className='App'> 
       <BrowserRouter>
-        <header className="App-header">
-          <p>Meowcafe</p>
-
-          <span>       
-            <Link to="/contact">To Contact</Link>
-            <Link to="/">To Home</Link>
-          </span>
-        </header>
-
+      <header className='App-header'> 
+      <h1>MeowCafe</h1>
+      <nav>
+        <ul className='Nav-list'>  
+          <li><Link className='Link-nav' to="/contact">To Contact</Link></li>
+          <li><Link  className='Link-nav' to="/">To Home</Link></li>
+        </ul>
+      </nav> 
+      </header>
         <Routes>
           <Route path="/" element={<Home />}>Home</Route>
           <Route path="/contact" element = {<Contact />}>Contato</Route>
         </Routes>
+        
       </BrowserRouter>
     </div>
 
